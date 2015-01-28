79754dfc95a92c9bea10172824dbdc557ce0a8b7
Rocio Tovar <rocio.tovar@worldline.com>
2015-01-28 17:17:43 +0100

    InfoVC started

---
+++ tekrowedding/tekrowedding/ui/viewcontrollers/InfoVC.h
@@ -0,0 +1,13 @@
+//
+//  InfoVC.h
+//  tekrowedding
+//
+//  Created by Rocio Tovar on 28/1/15.
+//  Copyright (c) 2015 Rocio Tovar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoVC : UIViewController
+
+@end
---
+++ tekrowedding/tekrowedding/ui/viewcontrollers/InfoVC.m
@@ -0,0 +1,54 @@
+//
+//  InfoVC.m
+//  tekrowedding
+//
+//  Created by Rocio Tovar on 28/1/15.
+//  Copyright (c) 2015 Rocio Tovar. All rights reserved.
+//
+
+#import "InfoVC.h"
+#import <MapKit/MapKit.h>
+
+@interface InfoVC ()
+
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
+
+@implementation InfoVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self setLocation];
+}
+
+- (void)setLocation
+{
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+
+    [geocoder geocodeAddressString:@"Carretera c-31, km. 191, El Prat de Llobregat"
+                 completionHandler:^(NSArray *placemarks, NSError *error){
+         for (CLPlacemark * aPlacemark in placemarks) {
+             // Set map point
+             MKPointAnnotation *point = [MKPointAnnotation new];
+             [point setCoordinate:[[aPlacemark location] coordinate]];
+             [point setTitle:[aPlacemark locality]];
+
+             // Set map region
+             MKCoordinateRegion region = _mapView.region;
+             region.center = aPlacemark.region.center;
+             region.span.longitudeDelta = 1;
+             region.span.latitudeDelta = 1;
+
+             // Add point to the map
+             [_mapView setRegion:region animated:NO];
+             [_mapView addAnnotation:point];
+
+             // Select the PointAnnotation programatically
+             [_mapView selectAnnotation:point animated:YES];
+         }
+     }];
+}
+
+@end
