b0b99432e512dfda92458d04375b1c254d03b775
Rocio Tovar <rocio.tovar@worldline.com>
2015-01-28 12:54:45 +0100

    First commit

---
+++ tekrowedding/tekrowedding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tekrowedding
+//
+//  Created by Rocio Tovar on 28/1/15.
+//  Copyright (c) 2015 Rocio Tovar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tekrowedding/tekrowedding/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  tekrowedding
+//
+//  Created by Rocio Tovar on 28/1/15.
+//  Copyright (c) 2015 Rocio Tovar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
