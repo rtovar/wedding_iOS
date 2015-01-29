ceaf073dc2c65a34d49c8d2864006a690cc64e70
Rocio Tovar <rocio.tovar@worldline.com>
2015-01-29 17:16:30 +0100

    GuestsVC screen started.

---
+++ tekrowedding/tekrowedding/ui/viewcontrollers/GuestsVC.m
@@ -0,0 +1,13 @@
+//
+//  GuestsVC.m
+//  tekrowedding
+//
+//  Created by Rocio Tovar on 29/1/15.
+//  Copyright (c) 2015 Rocio Tovar. All rights reserved.
+//
+
+#import "GuestsVC.h"
+
+@implementation GuestsVC
+
+@end
---
+++ tekrowedding/tekrowedding/ui/views/GuestCell.m
@@ -0,0 +1,13 @@
+//
+//  GuestCell.m
+//  tekrowedding
+//
+//  Created by Rocio Tovar on 29/1/15.
+//  Copyright (c) 2015 Rocio Tovar. All rights reserved.
+//
+
+#import "GuestCell.h"
+
+@implementation GuestCell
+
+@end
